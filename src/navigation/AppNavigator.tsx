f1bfed334decdf06e3c2886b61c81353c52e5d7d
gregjose25131 <gregjose2513@gmail.com>
2025-04-10 23:10:36 -0400

    Replaced Welcome screen with Login as the initial route and cleaned up header title options across screens

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,115 +1,109 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import { COLORS } from '../styles/colors';
-import WelcomeScreen from '../screens/welcomeScreen';
-import LoginScreen from '../screens/loginScreen';
-import RegisterScreen from '../screens/registerScreen';
-import ForgotPasswordOneScreen from '../screens/forgotPasswordOneScreen';
-import ForgotPasswordTwoScreen from '../screens/forgotPasswordTwoScreen';
-import ForgotPasswordThreeScreen from '../screens/forgotPasswordThreeScreen';
-import FeedScreen from '../screens/feedScreen';
-import MyUser from '../screens/myUserScreen';
-import MyPostsUserScreen from '../screens/myPostsUserScreen';
-import EditUserScreen from '../screens/editUserScreen';
-import PasswordUserScreen from '../screens/passwordUserScreen';
-import PilotsUserScreen from '../screens/pilotsUserScreen';
+import { createStackNavigator } from "@react-navigation/stack";
+import React from "react";
+import EditUserScreen from "../screens/editUserScreen";
+import FeedScreen from "../screens/feedScreen";
+import ForgotPasswordOneScreen from "../screens/forgotPasswordOneScreen";
+import ForgotPasswordThreeScreen from "../screens/forgotPasswordThreeScreen";
+import ForgotPasswordTwoScreen from "../screens/forgotPasswordTwoScreen";
+import LoginScreen from "../screens/loginScreen";
+import MyPostsUserScreen from "../screens/myPostsUserScreen";
+import MyUser from "../screens/myUserScreen";
+import PasswordUserScreen from "../screens/passwordUserScreen";
+import PilotsUserScreen from "../screens/pilotsUserScreen";
+import RegisterScreen from "../screens/registerScreen";
+import { COLORS } from "../styles/colors";
 
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
   return (
     <Stack.Navigator
-      initialRouteName="Welcome"
+      initialRouteName="Login"
       screenOptions={{
         headerStyle: {
           backgroundColor: COLORS.background, // Header background equal to screen background
         },
         headerTintColor: COLORS.text, // Back button color (white)
         headerTitleStyle: {
-          fontFamily: 'Roboto',
+          fontFamily: "Roboto",
         },
       }}
     >
       <Stack.Screen
-        name="Welcome"
-        component={WelcomeScreen}
-        options={{ headerShown: false }} // Hide the header in WelcomeScreen
-      />
-      <Stack.Screen
         name="Login"
         component={LoginScreen}
         options={{
-          headerTitle: '', 
+          headerTitle: "",
         }}
       />
       <Stack.Screen
         name="Register"
         component={RegisterScreen}
         options={{
-          headerTitle: '', 
+          headerTitle: "",
         }}
       />
       <Stack.Screen
         name="ForgotOne"
         component={ForgotPasswordOneScreen}
         options={{
-          headerTitle: '', 
+          headerTitle: "",
         }}
       />
       <Stack.Screen
         name="ForgotTwo"
         component={ForgotPasswordTwoScreen}
         options={{
-          headerTitle: '', 
+          headerTitle: "",
         }}
       />
       <Stack.Screen
         name="ForgotThree"
         component={ForgotPasswordThreeScreen}
         options={{
-          headerTitle: '', 
+          headerTitle: "",
         }}
       />
       <Stack.Screen
         name="Feed"
         component={FeedScreen}
         options={{
-          headerTitle: '', 
+          headerShown: false,
         }}
       />
       <Stack.Screen
         name="MyUser"
         component={MyUser}
         options={{
-          headerTitle: '', 
+          headerTitle: "",
         }}
       />
       <Stack.Screen
         name="MyPosts"
         component={MyPostsUserScreen}
         options={{
-          headerTitle: '', 
+          headerTitle: "",
         }}
       />
       <Stack.Screen
         name="EditUser"
         component={EditUserScreen}
         options={{
-          headerTitle: '', 
+          headerTitle: "",
         }}
       />
       <Stack.Screen
         name="PasswordUser"
         component={PasswordUserScreen}
         options={{
-          headerTitle: '', 
+          headerTitle: "",
         }}
       />
       <Stack.Screen
         name="PilotsUser"
         component={PilotsUserScreen}
         options={{
-          headerTitle: '', 
+          headerTitle: "",
         }}
       />
     </Stack.Navigator>
