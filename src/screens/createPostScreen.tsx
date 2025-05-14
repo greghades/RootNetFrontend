1c20d87065913500e34f2a95bbfe48e3c97f23fe
25mathias25 <ricardoxdugarte@gmail.com>
2025-05-13 23:47:28 -0400

    cargar imagen y la borra

--- src/screens/createPostScreen.tsx
+++ src/screens/createPostScreen.tsx
@@ -128,6 +128,8 @@ const CreatePostScreen = () => {
           autoFocus
         />
       </View>
+
+      {/*//cargar imagen*/}
 {typeof selectedImage === 'string' && selectedImage.trim() !== '' && (
   <View style={{ position: 'relative', alignSelf: 'center', marginVertical: 10 }}>
     <Image
@@ -165,6 +167,9 @@ const CreatePostScreen = () => {
       >
         <TouchableOpacity style={styles.iconButton} onPress={handleImagePick}>
           <AntDesign name="picture" size={24} color={COLORS.text} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.iconButton}>
+          <AntDesign name="smileo" size={24} color={COLORS.text} />
         </TouchableOpacity>
         <TouchableOpacity style={styles.iconButton}>
           <AntDesign name="link" size={24} color={COLORS.text} />
